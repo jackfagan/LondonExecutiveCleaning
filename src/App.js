b68631080bacfd962dc151a71c82ca3c4991d86e
Jack Fagan <jackkfagan01@gmail.com>
2020-11-30 21:21:19 +0000

    Complete redesign

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import logo from './images/logo.PNG';
 import './App.scss';
+import virus from './images/virus.svg';
+import rating from './images/rating.svg';
+import calendar from './images/calendar.svg';
 
 function App() {
   return (
@@ -14,10 +17,70 @@ function App() {
         </div>
       </header>
       <div className="App-main-body">
-        <div>
+        <div className="service-block">
+          <div className="service-wrapper">
+            <img className="service-icon" src={virus} />
+            <div className="service-text">
+              <h3>Corona Virus Cleaning</h3>
+              <p>Corona Virus means clenaing services lorem ipsum dolor sit amet lorem ipsum dolor sit amet book now for covid safe spray clean</p>
+            </div>
+          </div> 
+
+          <div className="service-wrapper">
+            <img className="service-icon" src={calendar} />
+            <div className="service-text">
+              <h3>7 Day Availability</h3>
+              <p>Corona Virus means clenaing services lorem ipsum dolor sit amet lorem ipsum dolor sit amet book now for covid safe spray clean</p>
+            </div>          </div> 
+
+          <div className="service-wrapper">
+            <img className="service-icon" src={rating} />
+            <div className="service-text">
+              <h3>10/10 Customer Satisfaction</h3>
+              <p>Corona Virus means clenaing services lorem ipsum dolor sit amet lorem ipsum dolor sit amet book now for covid safe spray clean</p>
+            </div>
+          </div> 
+        </div> 
+        <div className="about-us">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+
+        <div className="contact-div">
+          <hr />
+          <h1>Contact Us</h1>
           <img src={logo} className="App-logo" alt="logo" />
           <div className="accordion">
-            <div className="row">
+          <form className="contact-form">
+                        <label>
+                          <div className="input-div">
+                            <input className="input is-d-block" type="text" placeholder="Full Name" name="name" />
+                          </div>
+                        </label>
+                        <label>
+                          <div className="input-div">
+                            <input className="input is-d-block" type="text" placeholder="Email" name="email" />
+                          </div>
+                        </label>
+                        <label>
+                          <div className="input-div">
+                            <input className="input is-d-block" type="tel" placeholder="Number" name="number" />
+                          </div>
+                        </label>
+                        <label>
+                          <div className="input-div">
+                            <input className="input is-d-block" type="text" placeholder="Company" name="company" />
+                          </div>
+                        </label>
+                        <label>
+                          <div className="input-div message-box">
+                            <textarea className="input is-d-block" type="textarea" placeholder="Message" name="message" />
+                          </div>
+                        </label>
+                        <input type="submit" value="Submit" />
+                      </form>
+            {/* <div className="row">
               <div className="col">
                 <div className="tabs">
                   <div className="tab">
@@ -61,11 +124,9 @@ function App() {
                   </div>
                 </div>
               </div>
-            </div>
+            </div> */}
           </div>
         </div>
-
-
       </div>
    </div>
   );
